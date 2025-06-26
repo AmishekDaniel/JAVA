2ae50e521bcd6ba4548130baff1cea1e92c4d9a2
AmishekDaniel <aamishekdaniel@gmail.com>
2025-06-27 00:55:10 +0530

    files and foldersare added

--- TwoDarray.java
+++ TwoDarray.java
@@ -1,17 +1,18 @@
 public class TwoDarray {
     public static void main(String[] args) {
-        int[][] a = {{1,2,3},{4,5,6},{7,8,9}};
-        int[][] b = new int[3][3];
+        int[][] a = {{1,2,3,3},{4,5,6,6},{7,8,9,9},{7,8,9,9}};
+        int[][] b = new int[4][4];
         for(int i=0;i<a.length;i++){
 
-            for(int j=0;j<a.length;j++){
+            for(int j=0;j<4;j++){
                 System.out.print(a[i][j]+" ");
                 b[i][j] = a[j][i];
             }
-            System.out.println("");
+            System.out.println(" ");
         }
-
+        System.out.println("================");
         System.out.println("after transpose ");
+        System.out.println("================");
 
         for (int i = 0; i < a.length; i++) {
 
